da08eca1a41a9d0837eb27b25498d5292bed84ad
Trinh Pham <trinh.pham@logigear.com>
2020-11-30 12:44:53 +0700

    Fix import db script

--- .dbimporter/importdb.sh
+++ .dbimporter/importdb.sh
@@ -2,12 +2,12 @@
 
 cd /dbimporter
 
-if [ -f 'imported.txt' ]; then
+if [ -f '/dbimporter/imported.txt' ]; then
     echo "DB is imported"
     exit 0
 fi
 
-if [ -f 'wwi.bak' ]; then
+if [ -f '/dbimporter/wwi.bak' ]; then
     echo "Sample DB exists"
 else
     echo "Download Sample DB"
@@ -22,5 +22,5 @@ cd /var/opt/mssql
 /opt/mssql-tools/bin/sqlcmd -S data-grid_db -U SA -P 'Password789' -Q 'RESTORE FILELISTONLY FROM DISK = "/var/opt/mssql/backup/wwi.bak"' | tr -s ' ' | cut -d ' ' -f 1-2
 /opt/mssql-tools/bin/sqlcmd -S data-grid_db -U SA -P 'Password789' -Q 'RESTORE DATABASE WideWorldImporters FROM DISK = "/var/opt/mssql/backup/wwi.bak" WITH MOVE "WWI_Primary" TO "/var/opt/mssql/data/WideWorldImporters.mdf", MOVE "WWI_UserData" TO "/var/opt/mssql/data/WideWorldImporters_userdata.ndf", MOVE "WWI_Log" TO "/var/opt/mssql/data/WideWorldImporters.ldf", MOVE "WWI_InMemory_Data_1" TO "/var/opt/mssql/data/WideWorldImporters_InMemory_Data_1"'
 /opt/mssql-tools/bin/sqlcmd -S data-grid_db -U SA -P 'Password789' -Q 'SELECT Name FROM sys.Databases'
-echo "DB is imported" > imported.txt
+echo "DB is imported" > /dbimporter/imported.txt
 echo "Sample DB imported"
